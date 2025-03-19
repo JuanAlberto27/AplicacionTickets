d207a8b033de90108d781c6ef2850ec75b7afb79
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-03-19 09:40:05 +0100

    He creado dos componentes para el front, "formulario" y "muestra".

---
+++ apps/front/src/app/formulario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [CommonModule],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css',
+})
+export class FormularioComponent {}
---
+++ apps/front/src/app/muestra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-muestra',
+  imports: [CommonModule],
+  templateUrl: './muestra.component.html',
+  styleUrl: './muestra.component.css',
+})
+export class MuestraComponent {}
