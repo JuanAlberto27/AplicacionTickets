53a105045b49480b82340d29550dfeaacb874316
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-03-20 08:44:08 +0100

    Modelo de ticket creado para referenciarlo en el service

--- apps/front/src/app/services/tickets.service.ts
+++ apps/front/src/app/services/tickets.service.ts
@@ -1,17 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Ticket } from '../models/ticket.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TicketsService {
 
-  private apiUrl = ''
+  private apiUrl = 'http://localhost:3000/api/tickets'
+  private http = inject(HttpClient);
 
-  constructor(private http: HttpClient) {}
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-  crearTicket() {
-
+  getTickets(): Observable<Ticket[]> {
+    return this.http.get<Ticket[]>(this.apiUrl);
   }
+
+  getTicket(id: string): Observable<Ticket> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Ticket>(url);
+  }
+
+  createTicket(ticket: Ticket): Observable<Ticket> {
+    return this.http.post<Ticket>(this.apiUrl, ticket, this.httpOptions);
+  }
+
+  updateTicket(id: string, ticket: Ticket): Observable<Ticket> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.put<Ticket>(url, ticket, this.httpOptions);
+  }
+
 }
